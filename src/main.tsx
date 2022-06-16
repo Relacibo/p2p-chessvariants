068d9cf7073d38bccd725349b14ad61020c24b55
Reinhard Bronner <reinhard_bronner@t-online.de>
2022-06-16 07:28:43 +0200

    formatting

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import { PersistGate } from 'redux-persist/integration/react'
+import { PersistGate } from "redux-persist/integration/react";
 import App from "./App";
 import { persistor, store } from "./app/store";
 import "./index.css";
