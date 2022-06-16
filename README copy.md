d51a7f75750322760cc4135949dccc83a718edf8
Reinhard Bronner <reinhard_bronner@t-online.de>
2022-06-16 07:27:34 +0200

    some vite related fixes

--- README copy.md
+++
@@ -1,3 +0,0 @@
-# p2p-chessvariants
-
-Maybe at some day you can play custom made chess variants with this
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import { PersistGate } from "redux-persist/integration/react";
+import { PersistGate } from 'redux-persist/integration/react'
 import App from "./App";
 import { persistor, store } from "./app/store";
 import "./index.css";
@@ -12,7 +12,7 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <BrowserRouter basename={import.meta.env.PUBLIC_URL}>
           <App />
         </BrowserRouter>
       </PersistGate>
