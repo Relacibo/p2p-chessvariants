0344314bc76821bb8f83e1d9ad83d04882a5d257
Reinhard Bronner <reinhard_bronner@t-online.de>
2023-08-06 22:10:54 +0200

    Maybe this?

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,6 @@ import { BrowserRouter } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
 import App from "./App";
 import { persistor, store } from "./app/store";
-import GoogleAutoSignin from "./features/auth/GoogleAutoSignin";
 import "./index.css";
 import { Loader, Paper } from "@mantine/core";
 
@@ -15,16 +14,16 @@ const root = createRoot(container!);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={<Loader />} persistor={persistor}>
-        <GoogleOAuthProvider
-          clientId={import.meta.env.VITE_GOOGLE_IDENTITY_CLIENT_ID}
-        >
-          <BrowserRouter basename={import.meta.env.BASE_URL}>
-            <Paper>Why the fuck doesn't this work?</Paper>
-            {/* <App /> */}
-          </BrowserRouter>
-        </GoogleOAuthProvider>
-      </PersistGate>
+      {/* <PersistGate loading={<Loader />} persistor={persistor}> */}
+      <GoogleOAuthProvider
+        clientId={import.meta.env.VITE_GOOGLE_IDENTITY_CLIENT_ID}
+      >
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
+          <Paper>Why the fuck doesn't this work?</Paper>
+          {/* <App /> */}
+        </BrowserRouter>
+      </GoogleOAuthProvider>
+      {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>,
 );
