734a6479cee76886328511796770ff046c11d995
Reinhard Bronner <reinhard_bronner@t-online.de>
2023-08-06 22:05:38 +0200

    test

--- src/main.tsx
+++ src/main.tsx
@@ -8,22 +8,23 @@ import App from "./App";
 import { persistor, store } from "./app/store";
 import GoogleAutoSignin from "./features/auth/GoogleAutoSignin";
 import "./index.css";
-import { Loader } from "@mantine/core";
+import { Loader, Paper } from "@mantine/core";
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={<Loader/>} persistor={persistor}>
-        {/* <GoogleOAuthProvider
+    {/* <Provider store={store}>
+      <PersistGate loading={<Loader />} persistor={persistor}>
+        <GoogleOAuthProvider
           clientId={import.meta.env.VITE_GOOGLE_IDENTITY_CLIENT_ID}
-        > */}
+        >
           <BrowserRouter basename={import.meta.env.BASE_URL}>
             <App />
           </BrowserRouter>
-        {/* </GoogleOAuthProvider> */}
+        </GoogleOAuthProvider>
       </PersistGate>
-    </Provider>
-  </React.StrictMode>
+    </Provider> */}
+    <Paper>Why the fuck doesn't this work?</Paper>
+  </React.StrictMode>,
 );
