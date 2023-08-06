b1d214f8a33085c4e629a82c28d0ea86c83744d9
Reinhard Bronner <reinhard_bronner@t-online.de>
2023-08-06 20:37:41 +0200

    fix if session not available

--- src/features/auth/authMiddleware.ts
+++ src/features/auth/authMiddleware.ts
@@ -5,12 +5,15 @@ import { invalidToken, selectSession } from "./authSlice";
 const authMiddleware: Middleware = (store) => (next) => (action) => {
   if (action.type === "persist/REHYDRATE") {
     next(action);
-    let state: RootState = store.getState();
-    let session = selectSession(state);
-    if (
+    const state: RootState = store.getState();
+    const session = selectSession(state);
+    if (!session) {
+      return;
+    }
+    const isTokenInvalid =
       session.state === "logged-in" &&
-      new Date(session.claims.exp) < new Date(Date.now())
-    ) {
+      new Date(session.claims.exp) < new Date(Date.now());
+    if (isTokenInvalid) {
       store.dispatch(invalidToken());
     }
     return;
