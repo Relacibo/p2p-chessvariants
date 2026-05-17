78491dc956ca0c99fd4be730676732fce4da847e
Reinhard Bronner <reinhard_bronner@t-online.de>
2026-05-18 00:25:09 +0200

    fix(build): remove deprecated esbuild polyfill for vite 8

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
 import * as dotenv from "dotenv";
 import { defineConfig, loadEnv } from "vite";
-import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
@@ -16,6 +15,9 @@ export default defineConfig(({ command, mode }) => {
     build: {
       target: "esnext",
     },
+    define: {
+      global: "globalThis",
+    },
     server: {
       open: env.BASE_URL,
       proxy: {
@@ -26,20 +28,6 @@ export default defineConfig(({ command, mode }) => {
           ws: false,
           rewrite: (path) => path.replace(/^\/api/, ""),
         },
-      },
-    },
-    optimizeDeps: {
-      esbuildOptions: {
-        // Node.js global to browser globalThis
-        define: {
-          global: "globalThis",
-        },
-        // Enable esbuild polyfill plugins
-        plugins: [
-          NodeGlobalsPolyfillPlugin({
-            buffer: true,
-          }),
-        ],
       },
     },
   };
